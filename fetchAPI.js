8a289ff39b7acf631700dee61762fd6d954905d6
JFK <antoine.cesbron.pro@gmail.com>
2023-04-30 18:27:37 +0200

    Formated, translated

--- fetchAPI.js
+++ fetchAPI.js
@@ -26,47 +26,47 @@ const lastValues = {};
 async function fetchPowerData() {
 
     const base64Credentials = Buffer.from(`${credentials.username}:${credentials.password}`).toString('base64');
-  
+
     try {
 
-      const response = await fetch('http://192.168.1.25/power', {
+        const response = await fetch('http://192.168.1.25/power', {
 
-        headers: {
-          'Authorization': `Basic ${base64Credentials}`,
-        },
-      });
-  
-      if (!response.ok) {
+            headers: {
+                'Authorization': `Basic ${base64Credentials}`,
+            },
+        });
 
-        console.error('Erreur de réponse de l\'API :', response.statusText);
-        return;
-      }
-  
-      const data = await response.json();
-      const now = new Date();
-      const timeString = now.toLocaleTimeString();
-  
-      for (const key in topics) {
+        if (!response.ok) {
 
-        if (data.hasOwnProperty(key)) {
-
-          const topicInfo = topics[key];
-          const value = data[key];
-  
-          if (lastValues[key] !== value) {
-
-            console.log(`[${timeString}] ${topicInfo.name}: ${value} ${topicInfo.unit}`);
-            lastValues[key] = value;
-          }
+            console.error('API response error:', response.statusText);
+            return;
         }
-      }
+
+        const data = await response.json();
+        const now = new Date();
+        const timeString = now.toLocaleTimeString();
+
+        for (const key in topics) {
+
+            if (data.hasOwnProperty(key)) {
+
+                const topicInfo = topics[key];
+                const value = data[key];
+
+                if (lastValues[key] !== value) {
+
+                    console.log(`[${timeString}] ${topicInfo.name}: ${value} ${topicInfo.unit}`);
+                    lastValues[key] = value;
+                }
+            }
+        }
     } catch (error) {
 
-      console.error('Erreur lors de la récupération des données de l\'API :', error);
+        console.error('Error retrieving data from API:', error);
     }
-  }
-  
-  const updateInterval = 1000;
+}
+
+const updateInterval = 1000;
 
 setInterval(fetchPowerData, updateInterval);
 
